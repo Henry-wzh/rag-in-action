aa427c2d3ee27396caabe68238dd5371a42a943b
henry <958976577@qq.com>
2025-06-24 09:47:57 +0800

    preRetrieval

--- 05-检索前处理-PreRetrieval/01-查询构建/构建元数据Filter/01-加载Youtube示例.py
+++ 05-检索前处理-PreRetrieval/01-查询构建/构建元数据Filter/01-加载Youtube示例.py
@@ -1,9 +1,9 @@
 from langchain_community.document_loaders import YoutubeLoader
-
+import pytube
 # 加载包含元数据的文档
-docs = YoutubeLoader.from_youtube_url(
-    "https://www.youtube.com/watch?v=zDvnAY0zH7U", add_video_info=True
-).load()
+video = YoutubeLoader.from_youtube_url(
+    "https://www.youtube.com/watch?v=zDvnAY0zH7U", add_video_info=True)
+docs = video.load()
 
 # 查看加载的第一个文档的元数据
 print(docs[0].metadata)
